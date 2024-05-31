7f17ac0a421baa15e029407e5967439bb11916f1
ikoma <ikoma.kazuya@jp.panasonic.com>
2024-05-31 12:24:10 +0900

    change pass

--- codes/calculation_ortools.py
+++ codes/calculation_ortools.py
@@ -257,7 +257,7 @@ def ORtools(instance_name, cust_name):
 
     # log_searchの内容をtxtファイルに保存する
     with open(
-        f"../results/simulation/{cust_name}/{instance_name}_log_ortools.txt", "wb"
+        f"results/simulation/{cust_name}/{instance_name}_log_ortools.txt", "wb"
     ) as file:
         file.write(log_search_content)
 
